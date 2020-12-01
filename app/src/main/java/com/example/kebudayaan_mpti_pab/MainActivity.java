8be9733e669f28290c438d61e2e6bc5ac631645d
alysianurkhalisha <74672041+alysianurkhalisha@users.noreply.github.com>
2020-12-01 09:49:32 +0700

    coba

--- app/src/main/java/com/example/kebudayaan_mpti_pab/MainActivity.java
+++ app/src/main/java/com/example/kebudayaan_mpti_pab/MainActivity.java
@@ -3,23 +3,25 @@ package com.example.kebudayaan_mpti_pab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private Object view;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setOnClickListener(new View.OnClickListener() {
-        });
     }
-
-   // public void onclick(View view) {
 }
+    //public void menu(View view) {
+       //Log.d(LOG_TAG, "button clicked");
+        //Intent intent = new Intent(this,MainActivity.class);
+        //startActivity(intent);
