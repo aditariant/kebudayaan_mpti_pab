a56cdc5a6d6d56e5415959751c171a4fb1539f1e
SallMarta <63784694+SallMarta@users.noreply.github.com>
2020-12-04 17:32:44 +0700

    acitivity 1. tambahan

--- app/src/main/java/com/example/kebudayaan_mpti_pab/activity_tarianAdat.java
+++ app/src/main/java/com/example/kebudayaan_mpti_pab/activity_tarianAdat.java
@@ -16,12 +16,12 @@ public class activity_tarianAdat extends AppCompatActivity {
         setContentView(R.layout.activity_tarian_adat);
     }
 
-
-    //tinggal diubah activity_tarianAdata.class ke activity yang diinginkan
+    //ubah per method public void tarianAdat nya. terus di activity tarian adat .xml dipasangin onClick di image view sesuai adatnya
 
     public void tarianAdat1 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_deskripsiBudaya.class);
 
+        //3 string ini diganti sesuai nama string per budayanya
         String judulAdat = getString(R.string.judul_tariAceh);
         String asalAdat = getString(R.string.asal_tariAceh);
         String isiAdat = getString(R.string.isi_tariAceh);
@@ -29,6 +29,7 @@ public class activity_tarianAdat extends AppCompatActivity {
         keTarianAdat.putExtra("judulAdats", judulAdat.toString());
         keTarianAdat.putExtra("asalAdats", asalAdat.toString());
         keTarianAdat.putExtra("isiAdats", isiAdat.toString());
+        //gambar per budaya. 'background1' diganti sesuai gambar per budayanya.
         keTarianAdat.putExtra("gambarAdats", R.drawable.background1);
 
         startActivity(keTarianAdat);
@@ -51,61 +52,97 @@ public class activity_tarianAdat extends AppCompatActivity {
 
     /*public void tarianAdat5 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat6 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat7 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat8 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat9 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat10 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat11 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat12 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat13 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat14 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat15 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }
 
     public void tarianAdat16 (View view){
         Intent keTarianAdat = new Intent(activity_tarianAdat.this, activity_tarianAdat.class);
+
+
+
         startActivity(keTarianAdat);
     }*/
 }
